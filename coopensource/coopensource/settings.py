ebda7ac35b79a07f04e3d83d1cd9c72650bcc6f9
davibrandao <78423321+davibrandao@users.noreply.github.com>
2023-10-08 16:38:46 -0300

    Fix

--- coopensource/coopensource/settings.py
+++ coopensource/coopensource/settings.py
@@ -15,7 +15,12 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+CORS_ALLOWED_ORIGINS = [
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:8080",
+    "http://127.0.0.1:9000",
+]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
